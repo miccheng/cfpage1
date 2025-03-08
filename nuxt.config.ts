c0f677b67543fcc440c9b4c5a229cd6fe7121ede
Michael Cheng <michael_cheng@tech.gov.sg>
2025-03-08 14:52:20 +0800

    Add Cloudflare pages modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  modules: ["nitro-cloudflare-dev"],
+  nitro: {
+    preset: "cloudflare-pages",
+  },
+  devtools: { enabled: true },
+});
